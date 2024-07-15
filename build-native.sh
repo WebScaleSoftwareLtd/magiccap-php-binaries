4aea97a945d1cc916a43c03448f5ef42749273e1
Astrid Gealer <astrid@gealer.email>
2024-07-15 12:01:00 +0100

    remove exif from builds

--- build-native.sh
+++ build-native.sh
@@ -4,7 +4,7 @@ rm -rf php-${PHP_VERSION} php-${PHP_VERSION}.tar.gz
 wget https://www.php.net/distributions/php-${PHP_VERSION}.tar.gz
 tar -xvzf php-${PHP_VERSION}.tar.gz
 cd php-${PHP_VERSION}
-./configure --disable-all --with-openssl --enable-ftp --enable-exif --enable-sockets --disable-posix --disable-cgi
+./configure --disable-all --with-openssl --enable-ftp --enable-sockets --disable-posix --disable-cgi
 make -j $(nproc --all)
 mv sapi/cli/php ../php
 cd ..
