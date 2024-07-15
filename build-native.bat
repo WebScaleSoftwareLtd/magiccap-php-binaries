d60eff32badedec466f08a76aa09f84e25cfc380
Astrid Gealer <astrid@gealer.email>
2024-07-15 12:43:23 +0100

    this maybe?

--- build-native.bat
+++ build-native.bat
@@ -8,5 +8,5 @@ SET PHP_SDK_VS=vs17
 SET PHP_SDK_ARCH=x64
 call c:\php-sdk\bin\phpsdk_deps.bat --update --branch master
 buildconf && configure --disable-all --with-openssl --enable-ftp --enable-sockets --disable-cgi --enable-cli && nmake
-SET DIST_ZIP_SNAP=php.zip
-nmake snap
+SET DIST_ZIP_DEV_PACK=php.zip
+nmake build-dist
