368e714d1673d172b25b02d387f935b2110c2f0b
Astrid Gealer <astrid@gealer.email>
2024-07-15 11:53:04 +0100

    set some php sdk bounds

--- build-native.bat
+++ build-native.bat
@@ -4,5 +4,7 @@ call phpsdk-vs17-x64.bat
 call c:\php-sdk\bin\phpsdk_buildtree.bat phpmaster
 git clone https://github.com/php/php-src.git && cd php-src
 git checkout php-%PHP_VERSION%
-call c:\php-sdk\bin\phpsdk_deps.bat --update --branch master -t vs17
+SET PHP_SDK_VS=vs17
+SET PHP_SDK_ARCH=x64
+call c:\php-sdk\bin\phpsdk_deps.bat --update --branch master
 uildconf && configure --disable-all --with-openssl --enable-ftp --enable-exif --enable-sockets --disable-posix --disable-cgi && nmake
