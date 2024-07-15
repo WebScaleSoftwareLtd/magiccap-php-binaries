dbeaa9738fd39eaf0e8b6f8dd68138bee719c608
Astrid Gealer <astrid@gealer.email>
2024-07-15 11:44:47 +0100

    add --branch vs17

--- build-native.bat
+++ build-native.bat
@@ -4,5 +4,5 @@ call phpsdk-vs17-x64.bat
 call c:\php-sdk\bin\phpsdk_buildtree.bat phpmaster
 git clone https://github.com/php/php-src.git && cd php-src
 git checkout php-%PHP_VERSION%
-call c:\php-sdk\bin\phpsdk_deps.bat --update --branch master
+call c:\php-sdk\bin\phpsdk_deps.bat --update --branch master --branch vs17
 uildconf && configure --disable-all --with-openssl --enable-ftp --enable-exif --enable-sockets --disable-posix --disable-cgi && nmake
