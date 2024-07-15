0a4f89814ca8dab69d68500ec05094c82390d315
Astrid Gealer <astrid@gealer.email>
2024-07-15 03:20:22 +0100

    maybe this works?

--- build-native.bat
+++ build-native.bat
@@ -1,8 +1,8 @@
 git clone https://github.com/php/php-sdk-binary-tools.git c:\php-sdk
 chdir /d c:\php-sdk
 call phpsdk-vs17-x64.bat
-phpsdk_buildtree phpmaster
+call c:\php-sdk\bin\phpsdk_buildtree.bat phpmaster
 git clone https://github.com/php/php-src.git && cd php-src
 git checkout php-%PHP_VERSION%
-phpsdk_deps --update --branch %PHP_VERSION%
+call c:\php-sdk\bin\phpsdk_deps.bat --update --branch %PHP_VERSION%
 uildconf && configure --disable-all --with-openssl --enable-ftp --enable-exif --enable-sockets --disable-posix --disable-cgi && nmake
