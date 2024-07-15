fefbe084109a9876d9765804c9145ff2c49d2abc
Astrid Gealer <astrid@gealer.email>
2024-07-15 11:39:12 +0100

    try master for the deps

--- build-native.bat
+++ build-native.bat
@@ -4,5 +4,5 @@ call phpsdk-vs17-x64.bat
 call c:\php-sdk\bin\phpsdk_buildtree.bat phpmaster
 git clone https://github.com/php/php-src.git && cd php-src
 git checkout php-%PHP_VERSION%
-call c:\php-sdk\bin\phpsdk_deps.bat --update --branch %PHP_VERSION%
+call c:\php-sdk\bin\phpsdk_deps.bat --update --branch master
 uildconf && configure --disable-all --with-openssl --enable-ftp --enable-exif --enable-sockets --disable-posix --disable-cgi && nmake
