c352f07a568f55871b28b027e54fc20cf05f5a2e
Astrid Gealer <astrid@gealer.email>
2024-07-15 03:03:57 +0100

    back to boomer

--- build-native.bat
+++ build-native.bat
@@ -1,9 +1,9 @@
 git clone https://github.com/php/php-sdk-binary-tools.git c:\php-sdk
-cd c:\php-sdk
+chdir /d c:\php-sdk
 git checkout php-sdk-2.2.0
 .\phpsdk-vc15-x64.bat
 phpsdk_buildtree phpmaster
 git clone https://github.com/php/php-src.git && cd php-src
-git checkout php-$env:PHP_VERSION
-phpsdk_deps --update --branch $env:PHP_VERSION
+git checkout php-%PHP_VERSION%
+phpsdk_deps --update --branch %PHP_VERSION%
 uildconf && configure --disable-all --with-openssl --enable-ftp --enable-exif --enable-sockets --disable-posix --disable-cgi && nmake
